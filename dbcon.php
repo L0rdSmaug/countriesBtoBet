61489247dd87441278ed134076db978285a872ec
Stefan Ilievski <stefan_ilievski994@hotmail.com>
2022-10-10 10:41:05 +0200

    Made connection to the database, inserted data from the API into the DB

---
+++ dbcon.php
@@ -0,0 +1,21 @@
+<?php 
+
+$connect = mysqli_connect("localhost", "root", "", "countries_btobet");
+
+if(!$connect) {
+    die("Connection Failed". mysqli_connect_error());
+}
+
+$jsonData = "https://restcountries.com/v2/all?fields=name,region,population";
+
+$data = file_get_contents($jsonData);
+
+$array = json_decode($data, true);
+
+foreach($array as $row) {
+    $query = "INSERT INTO countries(name, region, population) VALUES('".$row['name']."', '".$row['region']."', '".$row['population']."')";
+    $query_run = mysqli_query($connect, $query);
+}
+
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- Jquery -->
+    <script src="https://ajax.googleapis.com./ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Bootstrap ICONS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <title>Car Store</title>
+  </head>
+  <body>
