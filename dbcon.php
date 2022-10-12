d8e70a5beee596cc26d75eed0bba43b617c7e3b1
Stefan Ilievski <stefan_ilievski994@hotmail.com>
2022-10-12 10:46:50 +0200

    Added check in the database connection file to see whether or not there are records in the DB , if not, they will be added from the api

--- dbcon.php
+++ dbcon.php
@@ -6,7 +6,15 @@ if(!$connect) {
     die("Connection Failed". mysqli_connect_error());
 }
 
-/* $jsonData = "https://restcountries.com/v2/all?fields=name,region,population";
+$query = "SELECT name FROM countries";
+$query_run = mysqli_query($connect, $query);
+$result = mysqli_num_rows($query_run);
+
+//Checking to see if there are no records in the database, to insert them from the api.
+
+if($result == 0) {
+
+$jsonData = "https://restcountries.com/v2/all?fields=name,region,population";
 
 $data = file_get_contents($jsonData);
 
@@ -16,6 +24,5 @@ foreach($array as $row) {
     $query = "INSERT INTO countries(name, region, population) VALUES('".$row['name']."', '".$row['region']."', '".$row['population']."')";
     $query_run = mysqli_query($connect, $query);
 }
- */
-
+}
 ?>
