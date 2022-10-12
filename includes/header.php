0750414b773d39c360d5ab31a59667789a4aadb8
Stefan Ilievski <stefan_ilievski994@hotmail.com>
2022-10-12 13:17:52 +0200

    Installed Axios via CDN link in the header

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+
+         <!-- Axios -->
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.2/axios.min.js"></script>
+
      <!-- Jquery -->
     <script src="https://ajax.googleapis.com./ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 
